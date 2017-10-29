d9e7bcee0df0df49526c2f9afe5c9aed994e96cb
Jesus-2nta-2lahi <amiiir14.2@gmail.com>
2017-10-29 19:24:12 +0200

    a

---
+++ lab3/lab3(2).py
@@ -0,0 +1,6 @@
+import turtle
+turtle.register_shape('pentagon',((50,0),(50,-50),(25,-100),(0,-50),(0,0)))
+turtle.shape('pentagon')
+turtle.left(90)
+turtle.getshapes()
+turtle.mainloop()
---
+++ lab3/shapes1.py
@@ -0,0 +1,42 @@
+import turtle
+
+turtle.speed(100)
+turtle.pensize(5)
+
+
+
+#howmanysides=6
+#sidelength=120
+#degree=60
+#howmanysquares=18
+#angle=20
+#howmanyshapes=360/angle
+
+#for i in range(howmanysquares):
+	#turtle.left(angle)
+	#for i in range (howmanysides):
+	#	turtle.forward(sidelength)
+	#	turtle.left(degree)
+	
+for i in range (5):
+	if i==1:
+		turtle.color('purple')
+	if i==2:
+		turtle.color('blue')
+	if i==3:
+		turtle.color('pink')
+	if i==4:
+		turtle.color('red')
+	if i==5:
+		turtle.color('turquoise')	
+
+
+	turtle.forward(100)
+	turtle.right(144)
+
+
+turtle.mainloop()
+
+
+
+
