503aa2aab8399cc0e7c2c6ec43b41a7121d2dc6f
Jesus-2nta-2lahi <amiiir14.2@gmail.com>
2017-11-19 19:26:03 +0200

    lab5

---
+++ lab5/lab5(1).py
@@ -0,0 +1,20 @@
+from turtle import *
+import random
+
+colormode(255)
+
+class Square(Turtle):
+	def __init__(self, size):
+		Turtle.__init__(self)
+		self.shapesize(size)
+		self.shape("square")
+	def random_color(self):
+		r=random.randint(0,255)
+		g=random.randint(0,255)
+		b=random.randint(0,255)
+		self.color(r,g,b)
+
+s=Square(10)
+s.random_color()
+
+mainloop()
